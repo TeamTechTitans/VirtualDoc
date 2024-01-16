65ca27c17c2f1995db2415e66cdc1980a79a0444
hmsmiraz <hmsmiraz64729@gmail.com>
2024-01-16 17:08:25 +0600

    added img and title

--- src/components/TopResearch/TopResearch.jsx
+++ src/components/TopResearch/TopResearch.jsx
@@ -1,10 +1,31 @@
+import largeLeftBg from "../../assets/TopResearchImg/team-1.jpg";
+import decorImg from "../../assets/TopResearchImg/decor-2.png";
 
 const TopResearch = () => {
-    return (
+  return (
+    <div className="my-5">
+      {/* 1st section title with large picture */}
+      <div className="flex gap-10">
         <div>
-            <h2 className="text-center font-3xl text-teal-600">Top Research</h2>
+          <img src={largeLeftBg} alt="" className="p-10" />
         </div>
-    );
+        <div className="mt-28">
+          {/* for title */}
+          <div className="flex items-center justify-center gap-2">
+            <div>
+              <img src={decorImg} alt="" />
+            </div>
+            <div>
+              <h6 className="text-center text-base font-semibold text-[#22b6af] uppercase">
+                Top Research
+              </h6>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* 2nd section team members */}
+    </div>
+  );
 };
 
 export default TopResearch;
