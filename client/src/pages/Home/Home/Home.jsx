f453fececf9894cc6b0ec528138f17abd4272f8b
Parthpal <parthapradippal@gmail.com>
2024-01-16 23:53:03 +0530

    laboratoryServices and about research added and updated

--- client/src/pages/Home/Home/Home.jsx
+++ client/src/pages/Home/Home/Home.jsx
@@ -1,14 +1,16 @@
+import AboutResearch from "../../../components/AboutResearch/AboutResearch";
+import LaboratoryServices from "../../../components/LaboratoryServices/LaboratoryServices";
 import Location from "../Location/Location";
-import AboutResearch from "../../components/AboutResearch/AboutResearch";
-import LaboratoryServices from "../../components/LaboratoryServices/LaboratoryServices";
+
 
 const Home = () => {
       return (
             <div>
                   home
-                  <Location></Location>
                   <AboutResearch/>
                   <LaboratoryServices/>
+                  <Location></Location>
+
             </div>
       );
 };
