2ae02908034feac4e54b348b21fa30f8a2a4f545
Habibur Rahman <habiburrahmanofficial157@gmail.com>
2024-01-17 22:37:30 +0600

    color change

--- client/src/pages/Home/FaQueSection/Faq.jsx
+++ client/src/pages/Home/FaQueSection/Faq.jsx
@@ -8,13 +8,13 @@ const Faq = () => {
         <Disclosure>
           {({ open }) => (
             <>
-              <Disclosure.Button className="flex w-full justify-between rounded-lg   text-left text-lg md:text-xl font-semibold  py-5 shadow-xl ">
+              <Disclosure.Button className="flex w-full justify-between rounded-lg   text-left text-lg md:text-xl font-semibold px-3  py-5 shadow-xl bg-primary-teal text-white">
                 <span>How is The Test Used?</span>
                 <ChevronUpIcon
                   className={`${open ? "rotate-180 transform" : ""} h-5 w-5 `}
                 />
               </Disclosure.Button>
-              <Disclosure.Panel className=" pb-2 pt-4 text-sm text-gray-500">
+              <Disclosure.Panel className="px-4 pb-2 pt-4 text-base text-black rounded-md bg-light-teal">
                 Medical tests can help detect a condition, determine a
                 diagnosis, plan treatment, check to see if treatment is working,
                 or monitor the condition over time. A doctor may order these
@@ -28,13 +28,13 @@ const Faq = () => {
         <Disclosure as="div" className="mt-9  shadow-xl ">
           {({ open }) => (
             <>
-              <Disclosure.Button className="flex w-full justify-between rounded-lg   text-left text-xl font-semibold  py-5 ">
+              <Disclosure.Button className="flex w-full justify-between rounded-lg   text-left text-xl font-semibold px-3 py-5 bg-primary-teal text-white">
                 <span>What does The Test Result Mean?</span>
                 <ChevronUpIcon
                   className={`${open ? "rotate-180 transform" : ""} h-5 w-5 `}
                 />
               </Disclosure.Button>
-              <Disclosure.Panel className="px-4 pb-2 pt-4 text-sm text-gray-500">
+              <Disclosure.Panel className="px-4  pb-2 pt-4 text-base text-black rounded-md bg-light-teal">
                 Positive and Negative Tests: <br></br> Positive means whatever
                 the test was looking for was found. Negative means whatever the
                 test was looking for was not found. Inconclusive means "unsure."
@@ -47,13 +47,13 @@ const Faq = () => {
         <Disclosure as="div" className="mt-9  shadow-xl ">
           {({ open }) => (
             <>
-              <Disclosure.Button className="flex w-full justify-between rounded-lg   text-left text-xl font-semibold  py-5 ">
+              <Disclosure.Button className="flex w-full justify-between rounded-lg   text-left text-xl font-semibold px-3 py-5 bg-primary-teal text-white">
                 <span>Where Can I Get Tested?</span>
                 <ChevronUpIcon
                   className={`${open ? "rotate-180 transform" : ""} h-5 w-5 `}
                 />
               </Disclosure.Button>
-              <Disclosure.Panel className="px-4 pb-2 pt-4 text-sm text-gray-500">
+              <Disclosure.Panel className="px-4 pb-2 pt-4 text-base text-black rounded-md bg-light-teal">
                 Healthcare Providers: <br></br> Many healthcare providers offer
                 safe screening and testing for COVID-19. You can contact your
                 primary care doctor or local healthcare facility to inquire
