b62e75ed7201fbdbee65a49638fcddcb371a5531
habib-153 <h.r.sihab155@gmail.com>
2024-01-22 22:47:04 +0600

    Blog card created

--- client/src/pages/Blogs/Blog/Blog.jsx
+++ client/src/pages/Blogs/Blog/Blog.jsx
@@ -1,9 +1,19 @@
 import BlogBanner from "../Component/BlogBanner/BlogBanner";
+import BlogData from "../BlogData/BlogData";
+import SideBar from "../SideBar/SideBar";
 
 const Blog = () => {
     return (
         <div>
             <BlogBanner />
+            <div className="container mx-auto grid grid-cols-12 gap-4">
+                <div className="col-span-8">
+                    <BlogData />
+                </div>
+                <div>
+                    <SideBar />
+                </div>
+            </div>
         </div>
     );
 };
---
+++ client/src/pages/Blogs/Component/BlogBanner/BlogCard/BlogCard.jsx
@@ -0,0 +1,50 @@
+import { Button } from "@material-tailwind/react";
+
+const BlogCard = ({ blog }) => {
+  const {
+    id,
+    cover,
+    title,
+    description,
+    comments,
+    date,
+    month,
+    author,
+    hashtags,
+  } = blog;
+  return (
+    <div className="space-y-3 my-10 w-[90%]">
+      <div className="relative">
+        <div className="group">
+          <img loading="lazy" src={cover} alt="img" className="w-full  rounded-2xl" />
+          <div className="overlay rounded-2xl absolute top-0 left-0 w-full h-full bg-black opacity-0 group-hover:opacity-50  transition-opacity duration-500"></div>
+        </div>
+      </div>
+
+      <div className="flex gap-4">
+        <div className=" text-center rounded-full border-white w-[95px] h-[95px] border-[5px] bg-light-teal shadow-lg p-6 ">
+          <p className="font-bold text-primary-teal  text-3xl leading-none">
+            {date}
+          </p>
+          <p className="font-semibold">{month}</p>
+        </div>
+        <div className="space-y-2">
+          <h1 className="text-[30px] font-bold">{title}</h1>
+          <p className="text-sm font-normal text-gray-400">
+            by {author}, {comments} Comments
+          </p>
+          <p className="font-normal text-gray-700">
+            {description.length < 40
+              ? description
+              : `${description.slice(0, 150)}..`}
+          </p>
+          <Button className="rounded-full" color="cyan">
+            Read More
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default BlogCard;
