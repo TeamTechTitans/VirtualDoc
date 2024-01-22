becb608ce86d2448deaeef02fdb98028f363a34a
jibon-roy <mailme.jibon@gmail.com>
2024-01-23 01:49:40 +0600

    doctors add

--- client/src/pages/doctors/Doctors.jsx
+++ client/src/pages/doctors/Doctors.jsx
@@ -1,10 +1,14 @@
 import SiteHeader from "../../components/siteHeader/SiteHeader";
+import FilterDoctor from "./components/FilterDoctor";
 
 
 const Doctors = () => {
     return (
         <div>
             <SiteHeader title={'- Most Experienced'}>Our Doctors</SiteHeader>
+            <div className="container mx-auto">
+                <FilterDoctor></FilterDoctor>
+            </div>
         </div>
     );
 };
