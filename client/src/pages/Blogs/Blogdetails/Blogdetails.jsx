8ab5b52a31184e3afe80f7878468582740442342
habib-153 <h.r.sihab155@gmail.com>
2024-01-24 23:40:43 +0600

    tags and logo

--- client/src/pages/Blogs/Blogdetails/Blogdetails.jsx
+++ client/src/pages/Blogs/Blogdetails/Blogdetails.jsx
@@ -1,6 +1,12 @@
 import { useLoaderData, useLocation, useParams } from "react-router-dom";
 import SideBar from "../SideBar/SideBar";
 import BlogBanner from "../Component/BlogBanner/BlogBanner";
+import {
+  FaFacebook,
+  FaInstagram,
+  FaPinterest,
+  FaTwitter,
+} from "react-icons/fa";
 
 const BlogDetails = () => {
   const location = useLocation();
@@ -35,16 +41,57 @@ const BlogDetails = () => {
               />
             </div>
             <div className="space-y-4">
-            <p className="my-4 font-normal text-gray-700">{blog.description}</p>
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-  {
-    blog.images?.map((image, index) => (
-      <div className="w-full items-center" key={index}>
-        <img src={image} className="mx-auto" alt={`Image ${index + 1}`} />
-      </div>
-    ))
-  }
-</div>
+              <p className="my-4 font-normal text-gray-700">
+                {blog.description}
+              </p>
+              <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+                {blog.images?.map((image, index) => (
+                  <div className="w-full items-center" key={index}>
+                    <img
+                      src={image}
+                      className="mx-auto"
+                      alt={`Image ${index + 1}`}
+                    />
+                  </div>
+                ))}
+              </div>
+              <div className="flex items-center justify-between">
+                <div className="flex gap-2 my-8 text-[18px] font-medium">
+                  <p className="text-[#0c1529] ">Tags: </p>
+                  <div className="flex flex-wrap gap-1 text-[#808080]">
+                    {blog.hashtags.map((hashtag, index) => (
+                      <span key={index}>
+                        {hashtag}
+                        {index !== blog.hashtags.length - 1 && ","}
+                      </span>
+                    ))}
+                  </div>
+                </div>
+                <div>
+                  <ul className="flex gap-4 text-white">
+                    <li>
+                      <a href="">
+                        <FaTwitter className="size-11 bg-[#466ca9] rounded-full p-3" />
+                      </a>
+                    </li>
+                    <li>
+                      <a href="">
+                        <FaFacebook className="size-11 bg-[#dd4b39] rounded-full p-3" />
+                      </a>
+                    </li>
+                    <li>
+                      <a href="">
+                        <FaPinterest className="size-11 bg-[#222222] rounded-full p-3" />
+                      </a>
+                    </li>
+                    <li>
+                      <a href="">
+                        <FaInstagram className="size-11 bg-[#55acee] rounded-full p-3" />
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
             </div>
           </div>
         </div>
