263b32839f9078eabebc3cf1d498d16a12761755
Mohammed Rifatul Islam <mdrifatulislam08@gmail.com>
2024-01-29 23:25:55 +0600

    edit

--- client/src/components/dashboardNav/DashboardNav.jsx
+++ client/src/components/dashboardNav/DashboardNav.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import {
   Cog6ToothIcon,
   InboxIcon,
@@ -18,29 +17,6 @@ import {
 } from "@material-tailwind/react";
 import React from "react";
 import { Link } from "react-router-dom";
-import Logo from "../logo/Logo";
-=======
-import {
-    Cog6ToothIcon,
-    HomeIcon,
-    PowerIcon,
-    UserCircleIcon
-} from "@heroicons/react/24/solid";
-import {
-    Card,
-    Chip,
-    List,
-    ListItem,
-    ListItemPrefix,
-    ListItemSuffix,
-    Typography
-} from "@material-tailwind/react";
-import React from 'react';
-import { Link } from 'react-router-dom';
-import Logo from "../logo/Logo";
-
-
->>>>>>> 32679e509a8182085732ebf3a414e76f7e234eda
 
 const DashboardNav = () => {
   const [open, setOpen] = React.useState(0);
@@ -105,7 +81,6 @@ const DashboardNav = () => {
           </ListItem>
         </Link>
 
-<<<<<<< HEAD
         <hr className="my-2 border-blue-gray-50" />
         <ListItem>
           <ListItemPrefix>
@@ -143,97 +118,6 @@ const DashboardNav = () => {
       </List>
     </Card>
   );
-=======
-    const handleOpen = (value) => {
-        setOpen(open === value ? 0 : value);
-    };
-
-
-    return (
-        <Card className="h-screen overflow-auto w-full max-w-[20rem] p-4">
-            <div className="bg-white z-50 justify-between items-center">
-                <Link to='/'><Logo></Logo></Link>
-            </div>
-            <List>
-                <Link to={'/dashboard'}>
-                    <ListItem className="p-0">
-                        <Typography color="blue-gray" className="mr-auto p-2 font-normal">
-                            Dashboard
-                        </Typography>
-                    </ListItem>
-                </Link>
-
-                <Link to={'/dashboard/allUser'}>
-                    <ListItem className="p-0">
-                        <Typography color="blue-gray" className="mr-auto p-2 font-normal">
-                            All Users
-                        </Typography>
-                    </ListItem>
-                </Link>
-                <Link to={'/dashboard'}>
-                    <ListItem className="p-0">
-                        <Typography color="blue-gray" className="mr-auto p-2 font-normal">
-                            All Doctors
-                        </Typography>
-                    </ListItem>
-                </Link>
-                <Link to={'/dashboard/appoinment'}>
-                    <ListItem className="p-0">
-                        <Typography color="blue-gray" className="mr-auto p-2 font-normal">
-                            Appoinments
-                        </Typography>
-                    </ListItem>
-                </Link>
-                <Link to={'/dashboard/addDoctor'}>
-                    <ListItem className="p-0">
-                        <Typography color="blue-gray" className="mr-auto p-2 font-normal">
-                            Add a Doctor
-                        </Typography>
-                    </ListItem>
-                </Link>
-                <Link to={'/dashboard/userProfile'}>
-                    <ListItem className="p-0">
-                        <Typography color="blue-gray" className="mr-auto p-2 font-normal">
-                            Edit Profile
-                        </Typography>
-                    </ListItem>
-                </Link>
-
-                <hr className="my-2 border-blue-gray-50" />
-                <ListItem>
-                    <Link to="/" className="flex">
-                        <ListItemPrefix>
-                            <HomeIcon className="h-5 w-5" />
-                        </ListItemPrefix>
-                        Home
-                        {/* <ListItemSuffix>
-                            <Chip value="14" size="sm" variant="ghost" color="blue-gray" className="rounded-full" />
-                        </ListItemSuffix> */}
-                    </Link>
-                </ListItem>
-                <ListItem>
-                    <ListItemPrefix>
-                        <UserCircleIcon className="h-5 w-5" />
-                    </ListItemPrefix>
-                    Profile
-                </ListItem>
-                <ListItem>
-                    <ListItemPrefix>
-                        <Cog6ToothIcon className="h-5 w-5" />
-                    </ListItemPrefix>
-                    Settings
-                </ListItem>
-                <ListItem>
-                    <ListItemPrefix>
-                        <PowerIcon className="h-5 w-5" />
-                    </ListItemPrefix>
-                    Log Out
-                </ListItem>
-            </List>
-
-        </Card>
-    );
->>>>>>> 32679e509a8182085732ebf3a414e76f7e234eda
 };
 
 export default DashboardNav;
--- client/src/routers/MainRouter/MainRouter.jsx
+++ client/src/routers/MainRouter/MainRouter.jsx
@@ -8,86 +8,6 @@ import Login from "../../pages/Login/component/Login";
 import DoctorRegistration from "../../pages/Registration/components/DoctorRegistration";
 import Registration from "../../pages/Registration/components/Registration";
 import Services from "../../pages/Services/Services";
-<<<<<<< HEAD
-import UserAppoinment from "../../pages/dashboard/UserAppoinment/UserAppoinment";
-import Doctors from "../../pages/doctors/Doctors";
-import Tip from "../../pages/Tips/Tip/Tip";
-import TipDetails from "../../pages/Tips/Tipdetails/Tipdetails";
-import Team from "../../pages/Team/Team";
-import AboutUs from "../../pages/aboutUs/AboutUs";
-import Dashboard from "../../pages/dashboard/Dashboard";
-
-const MainRouter = createBrowserRouter([
-  {
-    path: "/",
-    element: <MainLayout />,
-    children: [
-      {
-        index: true,
-        element: <Home />,
-      },
-      {
-        path: "/doctors",
-        element: <Doctors />,
-      },
-      {
-        path: "tips",
-        element: <Tip></Tip>,
-      },
-      {
-        path: "tips/:id",
-        element: <TipDetails></TipDetails>,
-        //loader: ({params}) => fetch(`blogs.json/${params.id}`)
-      },
-      {
-        path: "/register",
-        element: <Registration />,
-      },
-      {
-        path: "/team",
-        element: <Team />,
-      },
-      {
-        path: "/services",
-        element: <Services />,
-      },
-      {
-        path: "/doctorRegister",
-        element: <DoctorRegistration />,
-      },
-      {
-        path: "/register",
-        element: <Registration />,
-      },
-      {
-        path: "/login",
-        element: <Login />,
-      },
-      {
-        path: "/aboutUs",
-        element: <AboutUs />,
-      },
-      {
-        path: "/contact",
-        element: <Contact />,
-      },
-    ],
-  },
-  {
-    path: "/dashboard",
-    element: <DashboardLayout />,
-    children: [
-      {
-        path: "/dashboard",
-        element: <Dashboard></Dashboard>,
-      },
-      {
-        path: "/dashboard/userappoinment",
-        element: <UserAppoinment></UserAppoinment>,
-      },
-    ],
-  },
-=======
 import Team from "../../pages/Team/Team";
 import Tips from "../../pages/Tips/Tip/Tip";
 import TipDetails from "../../pages/Tips/Tipdetails/Tipdetails";
@@ -182,7 +102,6 @@ const MainRouter = createBrowserRouter([
             },
         ]
     }
->>>>>>> 32679e509a8182085732ebf3a414e76f7e234eda
 ]);
 
 export default MainRouter;
