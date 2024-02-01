e561f90eaaa574873bc2f29b8f62b6b9f1ecdd50
habib-153 <h.r.sihab155@gmail.com>
2024-02-01 13:18:53 +0600

    get comments data from server

--- client/src/pages/Tips/Tipdetails/Comments/Comments.jsx
+++ client/src/pages/Tips/Tipdetails/Comments/Comments.jsx
@@ -1,17 +1,27 @@
 import { useForm } from "react-hook-form";
 import { IoMdSend } from "react-icons/io";
 import useAuth from "../../../../lib/hooks/useAuth";
-import moment from 'moment'
+import moment from "moment";
 import useAxiosPublic from "../../../../lib/hooks/useAxiosPublic";
+import Swal from "sweetalert2";
+import { useQuery } from "@tanstack/react-query";
 
 const Comments = ({ tip }) => {
   const { register, handleSubmit, reset } = useForm();
-  const {user} = useAuth()
-  const axiosPublic = useAxiosPublic()
-//  console.log(user?.photoURL)
+  const { user } = useAuth();
+  const axiosPublic = useAxiosPublic();
+  //  console.log(user?.photoURL)
+
+  const { data: comments = [] } = useQuery({
+    queryKey: ["comments"],
+    queryFn: async () => {
+      const res = await axiosPublic.get(`/comments/${tip.title}`);
+      return res.data;
+    },
+  });
 
   const onSubmit = async (data) => {
-    reset()
+    reset();
     const comment = {
       comment_id: tip.id,
       tipName: tip.title,
@@ -19,38 +29,42 @@ const Comments = ({ tip }) => {
       email: user?.email,
       date: moment().format("MMM Do YY"),
       comment: data.comment,
-      userPhoto: user?.photoURL
+      userPhoto: user?.photoURL,
+    };
+    const res = await axiosPublic.post("/comments", comment);
+    if (res.data) {
+      Swal.fire({
+        icon: "success",
+        title: "Thanks for your comment",
+        showConfirmButton: false,
+        timer: 1500,
+      });
     }
-    const res = await axiosPublic.post('/comments', comment)
-    console.log(res)
   };
 
   return (
     <div className="py-4 space-y-4">
       <h2 className="text-2xl font-semibold">Comments </h2>
       <form onSubmit={handleSubmit(onSubmit)}>
-      <h2 className="text-2xl font-semibold my-3">Leave a comment here </h2>      <div className="flex items-center gap-2">
-        <div className="flex-1">
-        <textarea 
-          type="text"
-          placeholder="Comment here"
-          {...register("comment", { required: true })}
-          className="input h-16 input-bordered focus:outline-0 w-full rounded-2xl"
-        />
-      </div>
-      <div className="">
-          <button type="submit">
-            <IoMdSend className="text-gray-500 text-3xl" />
-          </button>
+        <h2 className="text-2xl font-semibold my-3">Leave a comment here </h2>{" "}
+        <div className="flex items-center gap-2">
+          <div className="flex-1">
+            <textarea
+              type="text"
+              placeholder="Comment here"
+              {...register("comment", { required: true })}
+              className="input h-16 input-bordered focus:outline-0 w-full rounded-2xl"
+            />
+          </div>
+          <div className="">
+            <button type="submit">
+              <IoMdSend className="text-gray-500 text-3xl" />
+            </button>
+          </div>
         </div>
-      </div>
-      
-    </form>
-    <div>
-      
+      </form>
+      <div></div>
     </div>
-    </div>
-    
   );
 };
 
