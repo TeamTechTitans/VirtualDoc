85ea84ab14f4dafda6a90f025daad53cd294f9bc
Habibur Rahman <habiburrahmanofficial157@gmail.com>
2024-02-02 00:08:44 +0600

    cheange team page akn style change and responsive our page

--- client/src/pages/Services/DoctorsCard.jsx
+++ client/src/pages/Services/DoctorsCard.jsx
@@ -1,22 +1,22 @@
 import { Typography } from "@material-tailwind/react";
 import { FaFacebook, FaInstagram, FaTwitter } from "react-icons/fa";
 
-const DoctorsCard = () => {
+const DoctorsCard = ({ doctor }) => {
   return (
     <div className="flex relative mb-20 p-2 justify-center items-center">
       <div className="max-w-[300px] flex max-h-[320px] hover-before before:rounded-t-full before:rounded-bl-full before:rounded-br-[1000px] before:w-full before:h-full before:opacity-30 before:cursor-pointer before:scale-0 hover:before:scale-100 before:duration-500 before:rounded-2xl before:bg-black  aspect-square">
         <img
-          src="https://laborex.smartdemowp.com/wp-content/uploads/2021/05/team-9.jpg"
+          src={doctor?.img}
           className="aspect-square max-w-[300px] rounded-t-full rounded-bl-full max-h-[300px]"
           alt=""
           loading="lazy"
         />
         <div className="absolute bg-secondary-teal z-10 p-4  rounded-3xl shadow-md -bottom-20 translate-x-1/2 ">
           <Typography variant="h6" className="text-center">
-            Dr. Margot Robbie
+            {doctor?.name}
           </Typography>
           <Typography variant="paragraph" className="text-center">
-            PHD Student
+            {doctor?.position}
           </Typography>
           <Typography
             variant="paragraph"
