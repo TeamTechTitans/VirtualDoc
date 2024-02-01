1ddfa275f12642a8002109eb21f1c9a0cff5c523
habib-153 <h.r.sihab155@gmail.com>
2024-02-01 14:16:00 +0600

    comment section fully done and need to be logged in for post a comment

--- client/src/pages/Tips/Tipdetails/Comments/Comments.jsx
+++ client/src/pages/Tips/Tipdetails/Comments/Comments.jsx
@@ -5,6 +5,7 @@ import moment from "moment";
 import useAxiosPublic from "../../../../lib/hooks/useAxiosPublic";
 import Swal from "sweetalert2";
 import { useQuery } from "@tanstack/react-query";
+import profileImg from "../../../../assets/profile.png";
 
 const Comments = ({ tip }) => {
   const { register, handleSubmit, reset } = useForm();
@@ -12,7 +13,7 @@ const Comments = ({ tip }) => {
   const axiosPublic = useAxiosPublic();
   //  console.log(user?.photoURL)
 
-  const { data: comments = [] } = useQuery({
+  const { data: comments = [], refetch } = useQuery({
     queryKey: ["comments"],
     queryFn: async () => {
       const res = await axiosPublic.get(`/comments/${tip.title}`);
@@ -39,14 +40,16 @@ const Comments = ({ tip }) => {
         showConfirmButton: false,
         timer: 1500,
       });
+      refetch()
     }
   };
 
   return (
     <div className="py-4 space-y-4">
       <h2 className="text-2xl font-semibold">Comments </h2>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <h2 className="text-2xl font-semibold my-3">Leave a comment here </h2>{" "}
+      <h2 className="text-2xl font-semibold my-3">Leave a comment here </h2>{" "}
+      {
+        user ? (<form onSubmit={handleSubmit(onSubmit)}>
         <div className="flex items-center gap-2">
           <div className="flex-1">
             <textarea
@@ -57,13 +60,41 @@ const Comments = ({ tip }) => {
             />
           </div>
           <div className="">
+            
             <button type="submit">
               <IoMdSend className="text-gray-500 text-3xl" />
             </button>
           </div>
         </div>
-      </form>
-      <div></div>
+      </form>)
+      :
+      (
+        <p className="text-[#848484] text-xl">Please <a href="/login" className="text-primary-teal font-bold">Login</a> to post a comment</p>
+      )
+      }
+      
+      <div>
+        {comments.map((comment) => (
+          <div className="flex gap-4 flex-col md:flex-row" key={comment._id}>
+            <div className="w-[80px]">
+              {comment?.userPhoto ? (
+                <img className="rounded-full" src={comment.userPhoto} alt="" />
+              ) : (
+                <img className="rounded-full" src={profileImg} alt="" />
+              )}
+            </div>
+            <div className="space-y-2 flex-1">
+              <h1 className="text-[20px] md:text-[25px] font-bold">
+                {comment.name}
+              </h1>
+              <p className="font-medium text-gray-600">{comment.date}</p>
+              <p className="text-[#848484] text-lg">{comment.comment}</p>
+              <hr />
+            </div>
+            
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
