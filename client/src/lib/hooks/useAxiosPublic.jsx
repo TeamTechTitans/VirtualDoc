93fc6f9580e9d4ea45b51f06f2b380c0434a6f52
habib-153 <h.r.sihab155@gmail.com>
2024-02-03 23:17:41 +0600

    reset import in login page

--- client/src/lib/hooks/useAxiosPublic.jsx
+++ client/src/lib/hooks/useAxiosPublic.jsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 const axiosPublic = axios.create({
+    // baseURL: 'http://localhost:5000'
     baseURL: 'https://virtual-doc-backend.vercel.app'
 })
 const useAxiosPublic = () => {
