fe76cda80c76b643aa6907fed86306f51a8ba38f
habib-153 <h.r.sihab155@gmail.com>
2024-02-11 19:10:41 +0600

    admin can delete doctor from all doctor page

--- client/src/pages/dashboard/AllDoctor/AllDoctor.jsx
+++ client/src/pages/dashboard/AllDoctor/AllDoctor.jsx
@@ -1,26 +1,22 @@
 import { useQuery } from "@tanstack/react-query";
-import {
-  Button,
-  Dialog,
-  Card,
-  CardBody,
-  CardFooter,
-  Typography,
-  Input,
-  Checkbox,
-} from "@material-tailwind/react";
-// import ManageModal from "./ManageModal";
+import { Button, Card, Typography } from "@material-tailwind/react";
+import ManageModal from "./ManageModal";
 import useApiLink from "../../../lib/hooks/useApiLink";
 import { useState } from "react";
 import DashboardHeading from "../../../components/DashboardHeading/DashboardHeading";
+import useAxiosPublic from "../../../lib/hooks/useAxiosPublic";
+import Swal from "sweetalert2"
+import { useNavigate } from "react-router-dom";
 
 const AllDoctor = () => {
-    const [doctorData, setUserData] = useState([]);
+  const [doctorData, setDoctorData] = useState([]);
   const TABLE_HEAD = ["Personal Info", "Education", "Extra Info", "Action"];
-const apiLink = useApiLink()
+  const apiLink = useApiLink();
+  const axiosPublic = useAxiosPublic()
+  const navigate = useNavigate();
 
-const { data: doctorDetails = [] } = useQuery({
-    queryKey: ['doctors'],
+  const { data: doctorDetails = [], refetch } = useQuery({
+    queryKey: ["doctors"],
     queryFn: async () => {
       const res = await fetch(`${apiLink}/doctors`);
       const doctors = await res.json();
@@ -28,17 +24,39 @@ const { data: doctorDetails = [] } = useQuery({
     },
   });
 
-
   const classes = "p-4 border-b border-blue-gray-50";
-
+  const handleDeleteDoctor = (id) =>{
+    // console.log(id)
+    Swal.fire({
+        title: "Are you sure?",
+        text: "Do you want to delete this doctor from database?",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Yes, delete it!",
+      }).then(async (result) => {
+        if (result.isConfirmed) {
+          const deleteRes = await axiosPublic.delete(
+            `/doctor/${id}`
+          );
+           console.log(deleteRes)
+          if (deleteRes) {
+            Swal.fire("Deleted!", "Doctor has been removed from database", "success");
+            navigate("/dashboard/allDoctor");
+            refetch()
+          }
+        }
+      });
+  }
   //  Manage Modal
   const [open, setOpen] = useState(false);
   const handleOpen = (doctor) => {
-    setOpen(!open)
-    setUserData(doctor)
+    setOpen(!open);
+    setDoctorData(doctor);
   };
-    return (
-        <div className="flex p-2 flex-col">
+  return (
+    <div className="flex p-2 flex-col">
       <DashboardHeading title="All Doctor">Manage All Doctor</DashboardHeading>
       <Card className="w-full max-w-7xl overflow-auto">
         <table className="w-full table-auto  font-barlow">
@@ -60,54 +78,71 @@ const { data: doctorDetails = [] } = useQuery({
               ))}
             </tr>
           </thead>
-          <tbody >
-            {doctorDetails?.map((doctor, index) => <tr className="font-barlow" key={index}>
-              <td className={classes}>
-                <Typography
-                  variant="small"
-                  color="blue-gray"
+          <tbody>
+            {doctorDetails?.map((doctor, index) => (
+              <tr className="font-barlow" key={index}>
+                <td className={classes}>
+                  <Typography variant="small" color="blue-gray">
+                    <span className="font-semibold">Name: </span>
+                    {doctor?.name}
+                    <br />
+                    <span className="font-semibold">Email: </span>
+                    {doctor?.email}
+                    <br />
+                    <span className="font-semibold">Blood Group: </span>
+                    {doctor?.blood_group}
+                  </Typography>
+                </td>
+                <td className={classes}>
+                  <Typography variant="small" color="blue-gray">
+                    {doctor?.education}
+                  </Typography>
+                </td>
+                <td className={classes}>
+                  <Typography
+                    variant="small"
+                    color="blue-gray"
+                    //   className="text-center"
+                  >
+                    <span className="font-semibold">Location: </span>
+                    {doctor?.loc}
+                    <br />
+                    <span className="font-semibold">Category: </span>
+                    {doctor?.health_category}
+                    <br />
+                    <span className="font-semibold">Registration No: </span>
+                    {doctor?.registration}
+                    <br />
+                  </Typography>
+                </td>
+                <td className={classes}>
+                  <Button
+                    color="blue"
+                    onClick={() => handleOpen(doctor)}
+                    className="px-2 py-1 rounded-none mr-2 normal-case"
+                  >
+                    Manage
+                  </Button>
 
-                >
-                  <span className="font-semibold">Name: </span>{doctor?.name}<br />
-                  <span className="font-semibold">Email: </span>{doctor?.email}<br />
-                  <span className="font-semibold">Blood Group: </span>{doctor?.blood_group}
-                </Typography>
-              </td>
-              <td className={classes}>
-                <Typography
-                  variant="small"
-                  color="blue-gray"
-
-                >
-                  {doctor?.education}
-                </Typography>
-              </td>
-              <td className={classes}>
-                <Typography
-                  variant="small"
-                  color="blue-gray"
-                //   className="text-center"
-                >
-                  <span className="font-semibold">Location: </span>{doctor?.loc}<br />
-                  <span className="font-semibold">Category: </span>{doctor?.health_category}<br />
-                  <span className="font-semibold">Registration No: </span>{doctor?.registration}<br />
-
-                </Typography>
-              </td>
-              <td className={classes}>
-                <Button color="blue" onClick={() => handleOpen(doctor)} className="px-2 py-1 rounded-none mr-2 normal-case">Manage</Button>
-
-                <Button className="px-2 py-1 rounded-none normal-case" color="red">Delete</Button>
-              </td>
-            </tr>
-
-            )}
+                  <Button onClick={()=> handleDeleteDoctor(doctor._id)}
+                    className="px-2 py-1 rounded-none normal-case"
+                    color="red"
+                  >
+                    Delete
+                  </Button>
+                </td>
+              </tr>
+            ))}
           </tbody>
         </table>
       </Card>
-      {/* <ManageModal handleOpen={handleOpen} open={open} doctor={doctorData}></ManageModal> */}
-    </div >
-    );
+      <ManageModal
+        handleOpen={handleOpen}
+        open={open}
+        doctor={doctorData}
+      ></ManageModal>
+    </div>
+  );
 };
 
 export default AllDoctor;
---
+++ client/src/pages/dashboard/AllDoctor/ManageModal.jsx
@@ -0,0 +1,98 @@
+import React from "react";
+import {
+    Button,
+    Dialog,
+    Card,
+    CardBody,
+    CardFooter,
+    Typography,
+    Input,
+    Select,
+    Option,
+} from "@material-tailwind/react";
+import { useForm } from "react-hook-form";
+
+
+export default function ManageModal({ handleOpen, open, doctor }) {
+    const { register, handleSubmit } = useForm();
+
+    const onSubmit = async (data) => {
+        console.log(data);
+
+        // const imageFile = { image: data.image[0] };
+        // const res = await axios.post(image_hosting_api, imageFile, {
+        //     headers: {
+        //         'content-type': 'multipart/form-data'
+        //     }
+        // })
+        // if (res.data.success) {
+        //     console.log("Hello")
+        //     console.log(res.data)
+        // }
+    };
+
+    return (
+
+        <>
+            <Dialog
+                size="lg"
+                open={open}
+                handler={handleOpen}
+                className="bg-transparent shadow-none"
+            >
+                <Card className="mx-auto w-full overflow-auto">
+                    <form onSubmit={handleSubmit(onSubmit)} className="overflow-auto">
+                        <CardBody className="flex flex-col gap-4">
+                            <Typography variant="h4" className="text-center">
+                                Manage {doctor?.name + '\'s '} Data
+                            </Typography>
+                            <Typography
+                                className="mb-3 font-normal text-red-400 max-w-[40rem] mx-auto text-center"
+                                variant="paragraph"
+                                color="gray"
+                            >
+                                Currently managing <b>{doctor?.name + '\'s '}</b> data. Remember without doctor's permission or any kinds of emergency issue you should not change the doctor data.
+                            </Typography>
+
+                            <div className="flex flex-col grid-cols-2 gap-5 md:grid">
+                                <Input {...register('name')} label="Name" color="teal" defaultValue={doctor?.name} size="lg" />
+                                <Input {...register("email")} label="Email" color="teal" size="lg" defaultValue={doctor?.email} />
+                                <Input label="Location" color="teal" defaultValue={doctor?.loc} size="lg" />
+                                <Select
+
+                                    id="blood_group"
+                                    name="blood_group"
+                                    {...register('blood_group')}
+                                    color="teal"
+                                    label="Select Blood Group"
+                                    value={doctor?.blood_group}
+                                    onChange={(e) => e.target.value}
+                                >
+                                    <Option value="A+">A+</Option>
+                                    <Option value="A-">A-</Option>
+                                    <Option value="B+">B+</Option>
+                                    <Option value="B-">B-</Option>
+                                    <Option value="AB+">AB+</Option>
+                                    <Option value="AB-">AB-</Option>
+                                    <Option value="O+">O+</Option>
+                                    <Option value="O-">O-</Option>
+                                </Select>
+                                <Input  {...register('image')} label="Image Url" color="teal" defaultValue={doctor?.image} size="lg" />
+                                <Input  {...register('loc')} label="Location" color="teal" size="lg" defaultValue={doctor?.loc} />
+
+                            </div>
+                        </CardBody>
+                        <CardFooter className="pt-0 flex gap-5 flex-wrap">
+                            <Button type="submit" className="w-full max-w-[10rem] bg-red-500" fullWidth>
+                                Update
+                            </Button>
+                            <Button className="max-w-[10rem] bg-secondary-blue" onClick={handleOpen} fullWidth>
+                                Cancel
+                            </Button>
+                        </CardFooter>
+                    </form>
+                </Card>
+            </Dialog>
+        </>
+    )
+}
