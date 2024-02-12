94955a1d69f92cf6b29e6c9660fe6d05b671be11
habib-153 <h.r.sihab155@gmail.com>
2024-02-12 22:57:57 +0600

    add doctor page modified

--- client/src/pages/dashboard/AddDoctor/AddDoctor.jsx
+++ client/src/pages/dashboard/AddDoctor/AddDoctor.jsx
@@ -3,11 +3,14 @@ import axios from "axios";
 import { useForm } from "react-hook-form";
 import { FaImage } from "react-icons/fa6";
 import DashboardHeading from "../../../components/DashboardHeading/DashboardHeading";
+import useAxiosPublic from "../../../lib/hooks/useAxiosPublic";
+import Swal from "sweetalert2";
 const image_hosting_key = import.meta.env.VITE_IMAGE_HOSTING_KEY;
 const image_hosting_api = `https://api.imgbb.com/1/upload?key=${image_hosting_key}`;
 
 const AddDoctor = () => {
   const { register, handleSubmit } = useForm();
+  const axiosPublic = useAxiosPublic();
 
   const onSubmit = async (data) => {
     console.log(data);
@@ -19,9 +22,31 @@ const AddDoctor = () => {
       },
     });
     if (res.data.success) {
-      console.log("Hello");
-      console.log(res.data);
+      const image = res.data.data.display_url;
+      const doctorData = {
+        name: data.name,
+        email: data.email,
+        blood_group: data.blood_group,
+        loc: data.loc,
+        image: image,
+        education: data.education,
+        registration: data.registration,
+        health_category: data.health_category,
+        about: data.about,
+        password: "A@1234",
+        role: "doctor",
+      };
+      console.log(doctorData);
+      const response = await axiosPublic.post("/doctors/createDoctors", doctorData);
+    if (response) {
+      Swal.fire({
+        icon: "success",
+        title: "Doctor Added to database",
+        showConfirmButton: false,
+        timer: 1000,
+      });
     }
+  }
   };
   return (
     <div className="mt-8 ml-8">
@@ -62,9 +87,8 @@ const AddDoctor = () => {
           <select
             label="Category"
             color="teal"
-            
             defaultValue="default"
-            {...register("category", { required: true })}
+            {...register("health_category", { required: true })}
             className="input focus:outline-0 h-10 input-bordered"
           >
             <option value="default" disabled>
@@ -77,6 +101,58 @@ const AddDoctor = () => {
             <option value="brain">Brain</option>
           </select>
         </div>
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text font-semibold">Blood Group</span>
+          </label>
+          <select
+            label="Category"
+            color="teal"
+            defaultValue="default"
+            {...register("blood_group", { required: true })}
+            className="input focus:outline-0 h-10 input-bordered"
+          >
+            <option value="default" disabled>
+              Select Blood Group
+            </option>
+            <option value="A+">A+</option>
+            <option value="A-">A-</option>
+            <option value="B+">B+</option>
+            <option value="B-">B-</option>
+            <option value="AB+">AB+</option>
+            <option value="AB-">AB-</option>
+            <option value="O+">O+</option>
+            <option value="O-">O-</option>
+          </select>
+        </div>
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text font-semibold">Location</span>
+          </label>
+          <Input {...register("loc")} label="Location" color="teal" />
+        </div>
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text font-semibold">Registration No.</span>
+          </label>
+          <Input
+            {...register("registration")}
+            label="Registration No."
+            color="teal"
+          />
+        </div>
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text font-semibold">Education</span>
+          </label>
+          <Input {...register("education")} label="Education" color="teal" />
+        </div>
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text font-semibold">About</span>
+          </label>
+          <Input {...register("about")} label="About" color="teal" />
+        </div>
         <div className="form-control h-36 w-full border border-black cursor-pointer focus:border-primary-teal hover:border-primary-teal  rounded-lg my-10">
           <label
             htmlFor="image"
@@ -87,7 +163,7 @@ const AddDoctor = () => {
               id="image"
               {...register("image", { required: true })}
               type="file"
-              placeholder="Upload Your Photo"
+              placeholder="Upload Photo"
               className="input hidden pt-2 h-full"
             />
           </label>
