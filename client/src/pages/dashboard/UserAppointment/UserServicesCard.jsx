f89c63d3b7ca89808098907d0955e3ca86e7395a
Habibur Rahman <habiburrahmanofficial157@gmail.com>
2024-02-21 12:34:51 +0600

    profile page responsive and services page responsive

--- client/src/pages/dashboard/UserAppointment/UserServicesCard.jsx
+++ client/src/pages/dashboard/UserAppointment/UserServicesCard.jsx
@@ -1,8 +1,22 @@
 const UserServicesCard = () => {
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mx-2 gap-2 py-5">
-      <div className="flex justify-center items-center mx-auto gap-10 py-10">
-        <img src="https://i.ibb.co/3rv5zkj/microscope.png" alt="" />
+      <div className="flex justify-center items-center mx-auto gap-10 py-10 shadow-md ">
+        <img className="h-44 w-48 rounded-md" src="https://i.ibb.co/b6VpcJH/teeth.jpg" alt="" />
+        <div>
+          <h1 className="font-bold">Medical Research</h1>
+          <p>Excepteur sint ocecat pro dent sunt in culpa qui.</p>
+        </div>
+      </div>
+      <div className="flex justify-center items-center mx-auto gap-10 py-10 shadow-md ">
+        <img className="h-44 w-48 rounded-md" src="https://i.ibb.co/8YSXC60/skin.jpg" alt="" />
+        <div>
+          <h1 className="font-bold">Medical Research</h1>
+          <p>Excepteur sint ocecat pro dent sunt in culpa qui.</p>
+        </div>
+      </div>
+      <div className="flex justify-center items-center mx-auto gap-10 py-10 shadow-md ">
+        <img className="h-44 w-48 rounded-md" src="https://i.ibb.co/hdxYnsY/clen.jpg" alt="" />
         <div>
           <h1 className="font-bold">Medical Research</h1>
           <p>Excepteur sint ocecat pro dent sunt in culpa qui.</p>
