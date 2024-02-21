c6d1c067f849df5547c87128f898b99af10ebe8c
Habibur Rahman <habiburrahmanofficial157@gmail.com>
2024-02-21 11:40:05 +0600

    user services card and data

--- client/src/pages/dashboard/UserAppointment/OurTeeth.jsx
+++ client/src/pages/dashboard/UserAppointment/OurTeeth.jsx
@@ -1,17 +1,12 @@
 import TitleDashboard from "./TitleDashboard";
-import ClinicalServices from "../../Home/components/clinicalServices/ClinicalServices";
 import UserServicesCard from "./UserServicesCard";
 
 const OurTeeth = () => {
   return (
-    <div className="">
+    <div className="container mx-auto ">
       <TitleDashboard title="Available slotes for teeth orthodontics"></TitleDashboard>
       <div className="container mx-auto">
-        <div className=" grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mx-2 gap-5">
-          <UserServicesCard></UserServicesCard>
-          <UserServicesCard></UserServicesCard>
-          <UserServicesCard></UserServicesCard>
-        </div>
+        <UserServicesCard></UserServicesCard>
       </div>
     </div>
   );
--- client/src/pages/dashboard/UserAppointment/UserServicesCard.jsx
+++ client/src/pages/dashboard/UserAppointment/UserServicesCard.jsx
@@ -1,11 +1,12 @@
 const UserServicesCard = () => {
-  
   return (
-    <div className="flex justify-center items-center mx-auto gap-10 py-10">
-      <img src="https://i.ibb.co/3rv5zkj/microscope.png" alt="" />
-      <div>
-        <h1 className="font-bold">Medical Research</h1>
-        <p>Excepteur sint ocecat pro dent sunt in culpa qui.</p>
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mx-2 gap-2 py-5">
+      <div className="flex justify-center items-center mx-auto gap-10 py-10">
+        <img src="https://i.ibb.co/3rv5zkj/microscope.png" alt="" />
+        <div>
+          <h1 className="font-bold">Medical Research</h1>
+          <p>Excepteur sint ocecat pro dent sunt in culpa qui.</p>
+        </div>
       </div>
     </div>
   );
